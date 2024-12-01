1b23beddf295ebe974ba8ed766c926274a73a320
MatiasGonzaloAlvarez <alvarezmg1989@gmail.com>
2024-12-01 07:55:17 +0100

    Initial Commit

---
+++ PokeApiEx/PokeApiExApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiExApp.swift
+//  PokeApiEx
+//
+//  Created by Matias Alvarez on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
